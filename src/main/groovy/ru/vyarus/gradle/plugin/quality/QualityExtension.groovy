b59fa1f825c9dd68bda2184f53ebeeca0f0e5219
Vyacheslav Rusakov <vyarus@gmail.com>
2017-01-24 04:06:04 +0700

    fix codenarc version

--- src/main/groovy/ru/vyarus/gradle/plugin/quality/QualityExtension.groovy
+++ src/main/groovy/ru/vyarus/gradle/plugin/quality/QualityExtension.groovy
@@ -23,7 +23,7 @@ class QualityExtension {
     String checkstyleVersion = '7.4'
     String pmdVersion = '5.5.2'
     String findbugsVersion = '3.0.1'
-    String codenarcVersion = '0.26'
+    String codenarcVersion = '0.26.0'
     /**
      * Sets AnimalSniffer version.
      * Works only when 'ru.vyarus.animalsniffer' plugin applied.
