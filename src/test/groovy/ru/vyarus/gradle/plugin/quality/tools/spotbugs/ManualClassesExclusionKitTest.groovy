df79d2b516299d62f545bf7c0d4a5ddf23610561
Vyacheslav Rusakov <vyarus@gmail.com>
2020-10-10 06:09:52 +0700

    test manual spotbugs classes exclusion

---
+++ src/test/groovy/ru/vyarus/gradle/plugin/quality/tools/spotbugs/ManualClassesExclusionKitTest.groovy
@@ -0,0 +1,55 @@
+package ru.vyarus.gradle.plugin.quality.tools.spotbugs
+
+import org.gradle.testkit.runner.BuildResult
+import org.gradle.testkit.runner.TaskOutcome
+import ru.vyarus.gradle.plugin.quality.AbstractKitTest
+
+/**
+ * @author Vyacheslav Rusakov
+ * @since 10.10.2020
+ */
+class ManualClassesExclusionKitTest extends AbstractKitTest {
+
+    def "Check manual spotbugs exclusion"() {
+        setup:
+        build("""
+            plugins {
+                id 'groovy'
+                id 'ru.vyarus.quality'
+            }
+
+            quality {
+                checkstyle false
+                pmd false
+                strict false
+            }
+            
+            afterEvaluate {
+                tasks.withType(com.github.spotbugs.snom.SpotBugsTask).configureEach {
+                    classes = classes.filter { 
+                        !it.path.endsWith('Sample2.class')
+                    }
+                }
+            }
+
+            repositories {
+                jcenter() //required for testKit run
+            }
+            
+            dependencies {
+                implementation localGroovy()
+            }
+        """)
+
+        fileFromClasspath('src/main/java/sample/Sample.java', '/ru/vyarus/gradle/plugin/quality/java/sample/Sample.java')
+        fileFromClasspath('src/main/java/sample/Sample2.java', '/ru/vyarus/gradle/plugin/quality/java/sample/Sample2.java')
+
+        when: "run check task with java and groovy sources"
+        BuildResult result = run('check')
+
+        then: "spotbugs detect violations only in 1 file"
+        result.task(":check").outcome == TaskOutcome.SUCCESS
+        def output = result.output
+        output.contains "1 (0 / 1 / 0) SpotBugs violations were found in 1 files"
+    }
+}
