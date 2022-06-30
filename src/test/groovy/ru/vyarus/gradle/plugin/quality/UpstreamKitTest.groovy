d9b185aa116def43153a0c3d6c89c5b90e024c59
Vyacheslav Rusakov <vyarus@gmail.com>
2022-06-30 17:47:48 +0700

    update upstream test

--- src/test/groovy/ru/vyarus/gradle/plugin/quality/UpstreamKitTest.groovy
+++ src/test/groovy/ru/vyarus/gradle/plugin/quality/UpstreamKitTest.groovy
@@ -12,7 +12,7 @@ import org.gradle.testkit.runner.TaskOutcome
 class UpstreamKitTest extends AbstractKitTest {
 
 
-    public static final String GRADLE = '7.2'
+    public static final String GRADLE = '7.4.2'
 
     def "Check java checks"() {
         setup:
